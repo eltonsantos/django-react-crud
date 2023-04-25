f1b24426909363478afd266e73e697ebba7d556f
Elton Santos <eltonaxl@hotmail.com>
2023-04-25 04:09:02 -0300

    Create tasks and redirect and improve tasks api

--- frontend/src/pages/TasksFormPage.jsx
+++ frontend/src/pages/TasksFormPage.jsx
@@ -1,3 +1,41 @@
+import { useForm } from "react-hook-form";
+import { createTask } from "../api/tasks.api";
+import { useNavigate } from "react-router-dom";
+
 export function TasksFormPage() {
-  return "oi";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit(async (data) => {
+    const res = await createTask(data);
+    console.log(res);
+    navigate("/tasks");
+  });
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register("title", { required: true })}
+        />
+        {errors.title && <span>Title is required</span>}
+
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register("description", { required: true })}
+        ></textarea>
+        {errors.description && <span>Description is required</span>}
+
+        <button>Save</button>
+      </form>
+    </div>
+  );
 }
