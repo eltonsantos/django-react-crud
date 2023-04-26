0590eb638b822670fa4007db68a064690e9a84d2
Elton Santos <eltonaxl@hotmail.com>
2023-04-26 14:15:04 -0300

    Delete tasks

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 export function TaskCard({ task }) {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div
+      style={{ background: "black", cursor: "pointer" }}
+      onClick={() => {
+        navigate(`/tasks/${task.id}`);
+      }}
+    >
       <h1>{task.title}</h1>
       <p>{task.description}</p>
       <hr />
--- frontend/src/pages/TasksFormPage.jsx
+++ frontend/src/pages/TasksFormPage.jsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
-import { createTask } from "../api/tasks.api";
-import { useNavigate } from "react-router-dom";
+import { createTask, deleteTask } from "../api/tasks.api";
+import { useNavigate, useParams } from "react-router-dom";
 
 export function TasksFormPage() {
   const {
@@ -10,6 +10,7 @@ export function TasksFormPage() {
   } = useForm();
 
   const navigate = useNavigate();
+  const params = useParams();
 
   const onSubmit = handleSubmit(async (data) => {
     const res = await createTask(data);
@@ -36,6 +37,20 @@ export function TasksFormPage() {
 
         <button>Save</button>
       </form>
+
+      {params.id && (
+        <button
+          onClick={async () => {
+            const accepted = window.confirm("Are you sure?");
+            if (accepted) {
+              await deleteTask(params.id);
+              navigate("/tasks");
+            }
+          }}
+        >
+          Delete
+        </button>
+      )}
     </div>
   );
 }
